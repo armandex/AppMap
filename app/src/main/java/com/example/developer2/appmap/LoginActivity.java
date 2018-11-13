87e519e4ae5f2aaec6c84a6b34e2e94c9d77f969
lider <jvillanueva@ol.com.pe>
2018-11-13 17:21:25 -0500

    commit sync version 1.0

--- app/src/main/java/com/example/developer2/appmap/LoginActivity.java
+++ app/src/main/java/com/example/developer2/appmap/LoginActivity.java
@@ -3,11 +3,11 @@ package com.example.developer2.appmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class activity_test extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test);
+        setContentView(R.layout.activity_login);
     }
 }
